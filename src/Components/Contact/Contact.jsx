5c3c7e8b90a0400e0c243406dae053a001651c74
Rishitjain4874 <rishitjain4874@gmail.com>
2024-05-01 02:00:54 +0530

    Conact Spline elements added

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -1,9 +1,12 @@
+import Spline from '@splinetool/react-spline'
+
 function Contact() {
   return (
-    <div>
-      
+    <div className='h-screen flex items-center justify-center'>
+      <Spline scene="https://prod.spline.design/9FEw76inYzAmhbFV/scene.splinecode"/>
     </div>
   )
 }
 
 export default Contact
+
